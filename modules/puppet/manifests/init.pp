45ad374dcc8985658cd38decce3a4a9df3b58b3e
av <av@ubuntu>
2014-03-05 13:38:58 +0100

    update via git

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -3,5 +3,20 @@ class puppet {
 		source => 'puppet:///modules/puppet/papply.sh', 
 		mode => '0755',
 	}
-
+	file {'/usr/local/bin/pull-updates':
+		source => 'puppet:///modules/puppet/pull-updates.sh',
+		mode => '0755'
+	}
+	file {'/home/av/.ssh/ubuntugit':
+		source => 'puppet:///modules/puppet/ubuntu.priv',
+		owner => 'av',
+		mode => '0600',
+	}
+	cron { 'run-puppet':
+		ensure => 'present',
+		user => 'av',
+		command => '/usr/local/bin/pull-updates',
+		hour => '*',
+		minute => '*/10',
+	}
 }
